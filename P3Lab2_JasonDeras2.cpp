6ca2b915199efee8a495f282f5ff261cede9f432
JasonDeras <derasjason2@gmail.com>
2020-01-31 14:31:10 -0600

    Creacion del primer y segundo ejercicio

--- P3Lab2_JasonDeras2.cpp
+++ P3Lab2_JasonDeras2.cpp
@@ -3,6 +3,7 @@
 #include <array>
 
 using namespace std;
+void sort(int *arreglo,int);
 
 int menu(){
 
@@ -11,7 +12,8 @@ int menu(){
 	cout<<"Menu\n\n";
 	cout<<"1. Ejercicio 1"<<endl;
 	cout<<"2. Ejercicio 2"<<endl;
-	cout<<"3. Elercicio 3\n\n";
+	cout<<"3. Elercicio 3"<<endl;
+	cout<<"4. Salir\n\n";
 	cout<<"Ingrese una opcion: ";
 	cin>>opcion;
 	cout<<"\n\n\n";
@@ -19,34 +21,120 @@ int menu(){
 
 }//Fin del metodo del menu
 
+int mdc(int a,int b){
+
+        if (a<b) {
+            cout<<a;
+            return 0;
+        } else {
+            return 1+mdc(a,b-a);
+        }
+
+}//Fin del me maximo comun divisior
+
+void Ejercicio2(int size){
+
+	int* arreglo=new int[size];
+
+	for(int i=0;i<size;++i){
+		arreglo[i]=(rand()%99);
+	}//Fin del for del llenado 
+		
+
+	cout<<"Arreglo Original"<<endl;
+        for(int j=0;j<size;++j){
+		cout<<arreglo[j]<<endl;
+	}//Fin del for de la impresion
+          
+	sort(arreglo,size);
+	
+	cout<<"Arreglo ordenado"<<endl;
+
+        for(int j=0;j<size;++j){
+                cout<<arreglo[j]<<endl;
+        }//Fin del for de la impresion
+
+	delete arreglo[];
+}//Fin del metodo del segundo ejercicio
+
+void sort(int* arr, int n){
+        if (n == 1)
+            return;
+
+        for (int i=0; i<n-1; i++)
+            if (arr[i] > arr[i+1])
+            {
+                int temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+            }
+
+        sort(arr, n-1);
+
+}//Fin del metodo para ordenar el arreglo
+
 int main(){
-	int usuario=1;//Controla que sea repetitivo
 
+	int usuario;//Controla que sea repetitivo
+	
 
-	while(usuario==1){
+	do{
 
-		switch(menu()){
+		switch(usuario=menu()){
 
 			case 1:
+
+				int numero1;
+				int numero2;
+
+				cout<<"Inregres el primer numero: ";
+				cin>>numero1;
+				
+				while(numero1<0){
+					cout<<"EL numero no puede ser nagtivo\n\n";
+
+					cout<<"Ingrese el primer numero: ";
+					cin>>numero1;
+				}//Valida el primer numero
+
+				cout<<"Ingrese el segundo numero: ";
+				cin>>numero1;
+
+				while(numero1<=0){
+					cout<<"El numero no pueder cero o menor que cero\n\n";
+				       	cout<<"Ingrese el segundo numero: ";
+					cin>>numero2;	
+				}//Valida el segundo numero
+
+				mdc(numero1,numero2);
+
 			break;
 			
 			case 2:
+
+				int size;//Tamaño del arreglo
+
+				cout<<"Ingrese el size del arreglo: ";
+				cin>>size;
+
+				while(size<=1){
+					
+					cout<<"El tamaño del arreglo no es valido\n\n\n";
+
+					cout<<"Ingrese el tamaño del arreglo: ";
+					cin>>size;
+
+				}//Valida que el arreglo tiene mas elementos
+
+				Ejercicio2(size);
 			break;
 
 			case 3:
-			break;
-
-			default:
-				cout<<"Opcion no valida\n\n\n";
-			break;	
-		
-		
+			break;		
 		}//Fin del case de las opciones
 
+	}while(usuario!=4);
 
-		cout<<"Volver al menu[1-Si/2.-No]: ";
-		cin>>usuario;
-	}//Fin del while del menu
 	return 0;
 
 }//Fin del main
